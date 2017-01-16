f268b5d573688832bc71b1ae5725241d18dd9ae8
kevinsnijder <kevinsnijder9@hotmail.com>
2017-01-16 11:59:44 +0100

    GUI v1.0.0

--- Domotica/Domotica/obj/Debug/android/src/domotica/domotica/R.java
+++ Domotica/Domotica/obj/Debug/android/src/domotica/domotica/R.java
@@ -14,37 +14,41 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int abort=0x7f060018;
-        public static final int buttonChangePinState=0x7f06000a;
-        public static final int buttonConnect=0x7f060009;
-        public static final int editTextIPAddress=0x7f060007;
-        public static final int editTextIPPort=0x7f060008;
-        public static final int exit=0x7f060017;
-        public static final int relativeLayout1=0x7f060000;
-        public static final int textView1=0x7f060011;
-        public static final int textView2=0x7f060013;
-        public static final int textView3=0x7f060015;
-        public static final int textViewChangePinState=0x7f06000b;
-        public static final int textViewChangePinStateValue=0x7f06000c;
-        public static final int textViewDebug=0x7f06000f;
-        public static final int textViewDebugValue=0x7f060010;
-        public static final int textViewIPAddress=0x7f060005;
-        public static final int textViewIPPort=0x7f060006;
-        public static final int textViewSensor=0x7f06000d;
-        public static final int textViewSensorValue=0x7f06000e;
-        public static final int textViewServer=0x7f060003;
-        public static final int textViewServerConnect=0x7f060004;
-        public static final int textViewTimerState=0x7f060001;
-        public static final int textViewTimerStateValue=0x7f060002;
-        public static final int toggleButton0=0x7f060012;
-        public static final int toggleButton1=0x7f060014;
-        public static final int toggleButton2=0x7f060016;
+        public static final int abort=0x7f07001c;
+        public static final int buttonChangePinState=0x7f07000a;
+        public static final int buttonConnect=0x7f070009;
+        public static final int editTextIPAddress=0x7f070007;
+        public static final int editTextIPPort=0x7f070008;
+        public static final int exit=0x7f07001b;
+        public static final int relativeLayout1=0x7f070000;
+        public static final int relativeLayout2=0x7f070017;
+        public static final int relativeLayout3=0x7f070013;
+        public static final int textView1=0x7f070014;
+        public static final int textView2=0x7f070015;
+        public static final int textView3=0x7f070016;
+        public static final int textView4=0x7f070012;
+        public static final int textViewChangePinState=0x7f07000b;
+        public static final int textViewChangePinStateValue=0x7f07000c;
+        public static final int textViewDebug=0x7f07000f;
+        public static final int textViewDebugValue=0x7f070010;
+        public static final int textViewIPAddress=0x7f070005;
+        public static final int textViewIPPort=0x7f070006;
+        public static final int textViewSensor=0x7f07000d;
+        public static final int textViewSensorValue=0x7f07000e;
+        public static final int textViewServer=0x7f070003;
+        public static final int textViewServerConnect=0x7f070004;
+        public static final int textViewTimerState=0x7f070001;
+        public static final int textViewTimerStateValue=0x7f070002;
+        public static final int toggleButton0=0x7f070018;
+        public static final int toggleButton1=0x7f070019;
+        public static final int toggleButton2=0x7f07001a;
+        public static final int toolbar1=0x7f070011;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class menu {
-        public static final int menu=0x7f050000;
+        public static final int menu=0x7f060000;
     }
     public static final class string {
         public static final int abort=0x7f040012;
@@ -69,5 +73,8 @@ public final class R {
         public static final int sensor_value=0x7f04000e;
         public static final int timer_state_text=0x7f040001;
         public static final int timer_state_value=0x7f040002;
+    }
+    public static final class style {
+        public static final int Theme_Custom=0x7f050000;
     }
 }
